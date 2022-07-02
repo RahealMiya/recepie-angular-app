47c45d5ecc0a13ab6fd3c528ded80cd4e5b2a254
RahealMiya <rahealmiya328@gmail.com>
2022-07-02 15:53:35 +0530

    angular chcked

---
+++ src/app/addrecipie/addrecipie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addrecipie',
+  templateUrl: './addrecipie.component.html',
+  styleUrls: ['./addrecipie.component.css']
+})
+export class AddrecipieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddrecipieComponent } from './addrecipie/addrecipie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddrecipieComponent
   ],
   imports: [
     BrowserModule,
